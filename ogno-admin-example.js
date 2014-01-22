c4f4ab8d92222988e8f3ccddd7f5c2c155fa02ba
Matteo De Micheli <mat.demicheli@gmail.com>
2014-01-22 17:42:38 +0100

    First commit

---
+++ ogno-admin-example.js
@@ -0,0 +1,60 @@
+// Cars Collection
+Cars = new Meteor.Collection2('cars', {
+    'schema' : {
+        'name' : {
+            type : String
+        },
+        'company' : {
+            type: String
+        },
+        'brand-new' : {
+            type: Boolean
+        }
+    }
+});
+
+Cars.allow({
+    'insert' : function () { return true; },
+    'update' : function () { return true; },
+    'remove' : function () { return true; }
+});
+
+// Test Collection
+TestCollection = new Meteor.Collection2('test', {
+    schema : {
+        'name' : {
+            type : String
+        }
+    }
+});
+
+TestCollection.allow({
+    'insert' : function () { return true; },
+    'update' : function () { return true; },
+    'remove' : function () { return true; }
+});
+
+// Router Configuration
+Router.map(function () {
+    this.route('home', {
+        path: '/',
+        template: 'rootTemplate'
+    });
+});
+
+// Ogno Admin Configuration
+OgnoAdmin.config({
+    'prefix' : '/admin',
+    'auto' : true
+});
+
+// Ogno Admin extend structure
+OgnoAdmin.structure([
+    {
+        'weight' : 3,
+        'type' : 'custom',
+        'use' : 'customTemplate',
+        'icon' : 'pencil',
+        'menu-title' : 'Custom Menu Item'
+    }
+]);
